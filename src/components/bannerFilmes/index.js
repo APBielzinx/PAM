4b08f1d0aae51b3cf20339a537a0ee5dbb79a34d
APBielzinx <112442554+APBielzinx@users.noreply.github.com>
2023-04-04 11:09:13 -0300

    l

---
+++ src/components/bannerFilmes/index.js
@@ -0,0 +1,22 @@
+import {React, Fragment} from "react"
+import { Image, Text } from "react-native"
+import { Fragment } from "react";
+
+export default function BannerFilmes () {
+    return (
+
+    <>
+    
+        <Text> Em cartaz </Text>
+
+        <Image     />
+    
+    
+    
+    
+    
+    </>
+
+
+    );
+} 
