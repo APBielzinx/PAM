fe42366146745139a31927b3d8d427aca1be26b9
APBielzinx <112442554+APBielzinx@users.noreply.github.com>
2023-04-18 12:01:05 -0300

    fim

--- App.js
+++ App.js
@@ -1,24 +1,22 @@
-import { StyleSheet, Text, View, FlatList } from 'react-native';
-import BannerFilmes from './src/components/bannerFilmes';
-import Header from './src/components/header';
-import Search from './src/components/searchBar';
-import Filmes from './src/data/movies';
-
-
+import { StyleSheet, Text, View, FlatList } from "react-native";
+import BannerFilmes from "./src/components/bannerFilmes";
+import CardFilmes from "./src/components/cardFilmes";
+import Header from "./src/components/header";
+import Search from "./src/components/searchBar";
+import Filmes from "./src/data/movies";
 
 export default function App() {
   return (
     <View style={styles.container}>
-     <Header></Header>
-     <Search></Search>
-     <BannerFilmes></BannerFilmes>
-     <FlatList
-     data = {Filmes}
-     
-     
-     />
+      <Header></Header>
+      <Search></Search>
+      <BannerFilmes></BannerFilmes>
+      <FlatList
+        data={Filmes}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <CardFilmes></CardFilmes>}
 
-
+      />
     </View>
   );
 }
@@ -26,8 +24,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#141A29',
-    alignItems: 'center',
-   
+    backgroundColor: "#141A29",
+    alignItems: "center",
   },
 });
---
+++ src/components/cardFilmes/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Image, Text, TouchableOpacity } from "react-native";
+import styles from "./style";
+
+
+
+export default function CardFilmes() {
+
+  return (
+
+    <TouchableOpacity style = {styles.containerFilmes}>
+
+      <Image style = {styles.imagemFilmes} source={require(`../../Img/6.jpg`)} />
+
+      <Text style = {styles.tituloFilmes}> A procura da Felicidade</Text>
+
+      <Text style = {styles.notaFilmes}>7.3</Text>
+
+    </TouchableOpacity>
+
+  );
+
+}
