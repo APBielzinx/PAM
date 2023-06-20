0658ce8c135352825731fc57b6a3bd738b04254f
APBielzinx <112442554+APBielzinx@users.noreply.github.com>
2023-06-20 11:48:19 -0300

    a

--- projetoFilms/App.js
+++ projetoFilms/App.js
@@ -1,115 +1,16 @@
 import { StyleSheet, Text, View, FlatList } from "react-native";
 
-import BannerFilmes from "./src/components/bannerFilmes";
 
-import Filmes from "./src/data/movies";
-
-import Header from "./src/components/header";
-
-import SearchBar from "./src/components/searchbar";
-
-import CardMovies from "./src/components/cardFilmes";
-
-import CardItens from "./src/components/CardItens"
-
-import Series from "./src/data/series"
-
+import Routes from "./src/routes";
 
 
 export default function App() {
 
   return (
 
-    <View style={styles.container}>
+   <Routes>
 
-      <Header></Header>
-
-      <SearchBar></SearchBar>
-
-      <BannerFilmes></BannerFilmes>
-
-
-
-
-      <View style={{width:"90%"}}>
-
-      <FlatList
-
-      horizontal ={true}
-
-      showsHorizontalScrollIndicator={false}
-
-        data={Filmes}
-
-        keyExtractor={(item) => item.id}
-
-        renderItem={ ({item}) => (
-
-        
-
-        <CardMovies
-
-        
-
-        titulo = {item.nome}
-
-        nota = {item.nota}
-
-        imagem = {item.imagem}
-
-        />
-
-        )}
-
-      />
-
-      </View>
-
-      <View style={{width:"90%"}}>
-
-      <Text style = {{fontSize:25,
-                    color: 'white',
-                    fontWeight:'bold',
-                     marginRight: 75,
-                     textAlign:'center'}}> Series</Text>
-
-
-      <FlatList
-
-      horizontal ={true}
-
-      showsHorizontalScrollIndicator={false}
-
-        data={Series}
-
-        keyExtractor={(item) => item.id}
-
-        renderItem={ ({item}) => (
-
-        
-           
-
-
-        <CardItens
-
-        
-
-        titulo = {item.nome}
-
-        nota = {item.nota}
-
-        imagem = {item.imagem}
-
-        />
-
-        )}
-
-      />
-
-      </View>
-
-
-    </View>
+</Routes>
 
   );
 
